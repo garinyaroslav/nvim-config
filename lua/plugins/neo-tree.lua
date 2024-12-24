a85581d6bf9816872d7fd72cff97524d277d3ee9
Garin Yaroslav <garinyaroslav13@mail.ru>
2024-12-24 20:19:48 +0300

    some changes

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,4 +7,17 @@ return {
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  opts = {
+    window = {
+      position = "current", -- Открывать Neo-tree в текущем окне
+    },
+    filesystem = {
+      follow_current_file = true, -- Следовать за текущим файлом
+      filtered_items = {
+        hide_dotfiles = false, -- Показывать скрытые файлы
+        hide_gitignored = false, -- Если нужно также показывать файлы, игнорируемые Git
+      },
+      use_libuv_file_watcher = true, -- Обновление содержимого при изменении файлов
+    },
+  },
 }
--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -1,35 +1,3 @@
--- if not vim.g.neovide then
---   return {} -- do nothing if not in a Neovide session
--- end
---
--- return {
---   "AstroNvim/astrocore",
---   ---@type AstroCoreOpts
---   opts = {
---     options = {
---       opt = { -- configure vim.opt options
---         -- configure font
---         guifont = "FiraCode_Nerd_Font:h14:b",
---         -- line spacing
---         linespace = 0,
---       },
---       g = { -- configure vim.g variables
---         neovide_cursor_animation_length = 0.02,
---         neovide_cursor_trail_size = 0,
---         neovide_hide_mouse_when_typing = true,
---         neovide_refresh_rate = 144,
---         -- configure scaling
---         neovide_scale_factor = 0.9,
---         -- configure padding
---         neovide_padding_top = 0,
---         neovide_padding_bottom = 0,
---         neovide_padding_right = 0,
---         neovide_padding_left = 0,
---       },
---     },
---   },
--- }
-
 if not vim.g.neovide then return {} end
 
 ---@param scale_factor number
@@ -65,10 +33,10 @@ return {
 
       g = {
         neovide_increment_scale_factor = 0.1,
-        neovide_min_scale_factor = 0.7,
+        neovide_min_scale_factor = 0.6,
         neovide_max_scale_factor = 2.0,
-        neovide_initial_scale_factor = 0.9,
-        neovide_scale_factor = 0.9,
+        neovide_initial_scale_factor = 0.7,
+        neovide_scale_factor = 0.7,
 
         neovide_cursor_animation_length = 0.02,
         neovide_cursor_trail_size = 0,
