d24a703a7f863f4317bcf34c421a60e792ab71f0
garinyaroslav <garinyaroslav13@mail.ru>
2025-05-30 17:35:39 +0300

    changes

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,6 +2,22 @@ return {
   {
     "akinsho/toggleterm.nvim",
     version = "*",
-    opts = {},
+    opts = {
+      direction = "float",
+    },
+    -- config = function(_, opts)
+    --   require("toggleterm").setup(opts)
+    --
+    --   vim.api.nvim_set_keymap("t", "<C-><C-n>", "<C-\\><C-n>", { noremap = true, silent = true })
+    --
+    --   vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
+    --
+    --   vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
+    --     pattern = { "term://*toggleterm*" },
+    --     callback = function()
+    --       if vim.bo.buftype == "terminal" then vim.cmd "startinsert" end
+    --     end,
+    --   })
+    -- end,
   },
 }
