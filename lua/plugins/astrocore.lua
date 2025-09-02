3012c189daa831289aae7062ac332f2fa43fbad1
garinyaroslav <garinyaroslav13@mail.ru>
2025-09-02 14:48:31 +0300

    changes with tab space

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -34,6 +34,7 @@ return {
         wrap = false, -- sets vim.opt.wrap
         shiftwidth = 4,
         tabstop = 4,
+        softtabstop = 4,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
